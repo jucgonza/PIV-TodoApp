3ebb94183dad3d89c3d0211e2b4da984021074da
jucgonza <jucargr84@gmail.com>
2025-09-09 10:59:46 -0500

    feat(statistics): Adding statistics in the top of the page to show total tasks, amount of pending and completed tasks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css'
 import {TodoList} from './components/TodoList';
+import ArrowTrendingUpIcon from '@heroicons/react/24/outline/ArrowTrendingUpIcon'
+
 
 function App() {
 
@@ -8,6 +10,12 @@ function App() {
 
   return (
     <div className="mx-auto my-5 w-[90%] sm:w-[80%] max-w-[1000px]">
+      <p className="my-2 flex items-center gap-2">
+        <ArrowTrendingUpIcon className="size-5 text-gray-600"/>
+        <span className="text-gray-800">
+          {todos.filter(i => i.completed).length} completed, {todos.filter(i => !i.completed).length} pending out of {todos.length} tasks
+        </span>
+      </p>
       <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   )
